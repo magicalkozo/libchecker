db3b7402d7f68d59569e9e0b10a33c0cd78e96c2
magicalkozo <138632305+magicalkozo@users.noreply.github.com>
2023-07-23 07:52:38 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,13 +2,7 @@
 
 int main(void)
 {
-    set_m32(1000000007);
-    u32 a, b;
-    for (int _ = 0; _ < 100000; _++)
-    {
-        a = pcg_range(1, 1000000);
-        b = pcg_range(1, 1000000);
-        assert(add_m32(a, b) == shrink_add_m32(a, b));
-    }
+    u32 a = pcg_range(1, 10);
+    assert(a > 100);
     return 0;
 }
