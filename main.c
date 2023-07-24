7eda79ccef608f119ad79c0b492cb98b52adc50d
magicalkozo <138632305+magicalkozo@users.noreply.github.com>
2023-07-25 08:31:54 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(void)
 {
     set_m32(998244353u);
     u32 a, b;
-    for (int _ = 0; _ < 10000000000; _++)
+    for (int _ = 0; _ < 100; _++)
     {
         a = pcg_range(0, 998244352u);
         b = pcg_range(0, 998244352u);
