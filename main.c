70e7b5c94652ecfac43d591f0004ae1c5da900bf
magicalkozo <138632305+magicalkozo@users.noreply.github.com>
2023-07-25 23:58:38 +0900

    update

--- main.c
+++ main.c
@@ -2,13 +2,5 @@
 
 int main(void)
 {
-    set_m32(998244353u);
-    u32 a, b;
-    for (int _ = 0; _ < 100; _++)
-    {
-        a = pcg_range(0, 998244352u);
-        b = pcg_range(0, 998244352u);
-        assert(add_m32(a, b) == (a + b) % 998244353u);
-    }
     return 0;
 }
