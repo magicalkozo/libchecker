bf639ae32fc1dcd68b635d715313cf4c891e2008
magicalkozo <138632305+magicalkozo@users.noreply.github.com>
2023-07-26 09:51:04 +0900

    f

--- main.c
+++ main.c
@@ -2,14 +2,6 @@
 
 int main(void)
 {
-    u32 mod = 998244353u;
-    set_m32(mod);
-    for (int _ = 0; _ < 1000; _++)
-    {
-        u32 a = pcg_range(0u, 998244352u);
-        u32 b = pcg_range(0u, 998244352u);
-        u32 B = to_m32(b);
-        assert(mul_m32(a, b) == mul_m32(a, B));
-    }
+    __builtin_trap();
     return 0;
 }
